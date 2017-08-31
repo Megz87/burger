4c6aafe25960cc376508ca39eba1b99f48211145
MEGAN ZAHARONI <meganlanierz9@gmail.com>
2017-08-31 15:09:03 -0700

    seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ("bacon-double cheeseburger", true);
+INSERT INTO burgers (burger_name, devoured, date) VALUES ("bacon-double cheeseburger", true);
 INSERT INTO burgers (burger_name, devoured) VALUES ("avocado-mushroom burger", true);
 INSERT INTO burgers (burger_name, devoured) VALUES ("bison double double", true);
 INSERT INTO burgers (burger_name, devoured) VALUES ("bison cheesy onion burger", true);
